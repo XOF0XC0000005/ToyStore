bf7e0bb694eb8bd46e92a9942777db623352c192
XOF0XC0000005 <leva95reva@yahoo.com>
2023-12-21 10:26:32 +0300

    Toy
    create toy class

---
+++ ToyStore/src/Toy.java
@@ -0,0 +1,10 @@
+public class Toy {
+    private String id;
+    private String name;
+    private int rate;
+    public Toy(String id, String name, int rate) {
+        this.id = id;
+        this.name = name;
+        this.rate = rate;
+    }
+}
